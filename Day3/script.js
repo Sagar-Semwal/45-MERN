b6960110edd33acf32f0e930ac5c3768acbe690c
Sagar-Semwal <sagarsemwal7july@gmail.com>
2025-09-03 08:13:38 +0530

    Day-3 (JS CaculateGrade Function)

---
+++ Day3/script.js
@@ -0,0 +1,22 @@
+function calculateGrade(score) {
+  if (score >= 90 && score <= 100) {
+    return 'A';
+  } else if (score >= 80 && score < 90) {
+    return 'B';
+  } else if (score >= 70 && score < 80) {
+    return 'C';
+  } else if (score >= 60 && score < 70) {
+    return 'D';
+  } else if (score >= 0 && score < 60) {
+    return 'F';
+  } else {
+    return 'Invalid score';
+  }
+}
+
+console.log(calculateGrade(95));
+console.log(calculateGrade(82));
+console.log(calculateGrade(76));
+console.log(calculateGrade(64));
+console.log(calculateGrade(50));
+console.log(calculateGrade(110));
